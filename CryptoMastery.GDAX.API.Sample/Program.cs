ee05b76a85533764e850ab4261307814d4b7bb6a
Alexander Shkabarnya <ashkabarnya@gmail.com>
2018-02-25 23:32:08 -0500

    Sample added

--- CryptoMastery.GDAX.API.Sample/Program.cs
+++ CryptoMastery.GDAX.API.Sample/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.Threading.Tasks;
+using CryptoMastery.GDAX.API.Configuration;
+using CryptoMastery.GDAX.API.Model;
+using CryptoMastery.GDAX.API.PrivateServices;
+using CryptoMastery.GDAX.API.PublicServices;
 
 namespace CryptoMastery.GDAX.API.Sample
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Sample Started");
+                await Sample();
+                Console.WriteLine("Sample Complete");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occured: {ex.Message}");
+            }
+        }
+
+        private static async Task Sample()
+        {
+            var settingsProvider = new ConnectionSettingsProvider(ConnectionSettingsProvider.DefaultSandbox);
+            var credentailsProvider = new SampleCredentailsProvider();
+
+            //Currencies
+            var currencyService = new CurrenciesService(settingsProvider);
+            var currencies = await currencyService.GetCurrenciesAsync();
+            Console.WriteLine($"{currencies.Count} currencies found");
+
+            //Products
+            var prdService = new ProductsService(settingsProvider);
+            var products = await prdService.GetProductsAsyc();
+            Console.WriteLine($"{products.Count} products found");
+            var productTicker = await prdService.GetProductTickerAsyc("BTC-USD");
+            Console.WriteLine($"Latest price for BTC-USD is {productTicker.Price}");
+
+            //Time
+            var timeService = new TimeService(settingsProvider);
+            var time = await timeService.GetTimeAsync();
+            Console.WriteLine($"Service time: {time.ServerTime}");
+
+            //Accounts
+            var acctService = new AccountsService(credentailsProvider, settingsProvider);
+            var accts = await acctService.GetAccountsAsync();
+            Console.WriteLine($"{currencies.Count} accounts found");
+            if (accts.Count > 0)
+            {
+                var acct = await acctService.GetAccountByIdAsync(accts[0].Id);
+                Console.WriteLine($"Account with id: {acct.Id} retrieved");
+                var acctHolds = await acctService.GetAccountHoldsAsync(accts[1].Id);
+                Console.WriteLine($"{acctHolds.Count} account holds found for account {accts[1].Id}");
+            }
+
+            //Orders
+            var orderService = new OrdersService(credentailsProvider, settingsProvider);
+            //Buy BTC
+            var order = await orderService.PlaceLimitGTCOrderAsync(Sides.Buy, Product.BTC_USD, 9000m, 0.01m);
+            Console.WriteLine($"Order with id {order.Id} has been placed");
+            var orders = await orderService.GetOrdersAsync();
+            Console.WriteLine($"{orders.Count} orders found");
+            var ord1 = await orderService.GetOrderByIdAsync(order.Id);
+            Console.WriteLine($"Order with id: {ord1.Id} retrieved");
+
+            //Fills
+            var fillsService = new FillsService(credentailsProvider, settingsProvider);
+            var fills = await fillsService.GetFillsAsync(ord1.Id);
+            Console.WriteLine($"{fills.Count} fills found for order {ord1.Id}");
         }
     }
 }
---
+++ CryptoMastery.GDAX.API.Sample/SampleCredentailsProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CryptoMastery.GDAX.API.Contracts.Configuration;
+
+namespace CryptoMastery.GDAX.API.Sample
+{
+    public class SampleCredentailsProvider : ICredentialsProvider
+    {
+        public SampleCredentailsProvider()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("GDAX_API_KEY");
+            if (string.IsNullOrEmpty(apiKey))
+                throw new ApplicationException("GDAX_API_KEY environment variable is not set");
+
+            var secret = Environment.GetEnvironmentVariable("GDAX_SECRET");
+            if (string.IsNullOrEmpty(secret))
+                throw new ApplicationException("GDAX_SECRET environment variable is not set");
+
+            var passPhrase = Environment.GetEnvironmentVariable("GDAX_PASSPHRASE");
+            if (string.IsNullOrEmpty(passPhrase))
+                throw new ApplicationException("GDAX_PASSPHRASE environment variable is not set");
+
+            ApiKey = apiKey;
+            Secret = secret;
+            PassPhrase = passPhrase;
+        }
+
+        public string ApiKey { get; }
+        public string Secret { get; }
+        public string PassPhrase { get; }
+    }
+}
